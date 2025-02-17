ecd3c8d0d13dbff58beb4a337948f9938e776d34
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-02-17 18:21:08 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import AllProducts from "../api/AllProducts"
+
+
+ function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <AllProducts />
+        </div>
+    )
+}
+
+export default Home;
