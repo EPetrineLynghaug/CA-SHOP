c1c605851c58f0684d9e789cab5891ca25cba400
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-03-10 20:42:23 +0100

    .

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,16 +2,14 @@ import { Outlet } from 'react-router';
 import Header from './header/Header';
 import Footer from './footer/Footer';
 
-
 const Layout = () => {
   return (
-    <div className="app-container">
+    <div className="app-container min-h-screen flex flex-col">
       <Header />
-      <main>
+      <main className="flex-grow">
         <Outlet />
       </main>
       <Footer />
-   
     </div>
   );
 };
